0e0fb83ad441848e8bfce9a378694c18ccd1a8b9
procode99 <web.poran@gmail.com>
2025-08-02 00:17:27 +0600

    add task for modiul 16

---
+++ task.js
@@ -0,0 +1,41 @@
+
+// task 1
+//Access the golden rod color value in output.
+// const colors = {
+//     red: "#ff0000",
+//     green: "#00ff00",
+//     blue: "#0000ff",
+//     "golden rod": '#daa520'
+// };
+
+
+// task 2
+//For this object below add a property named passenger capacity with value 5
+
+
+// const car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2020
+// };
+
+
+// task 3
+
+//Display the physics marks as output.
+// const student = {
+//     name: "Hamim Sakep",
+//     id: 5421,
+//     physics: {
+//         subject: "HSC Physics",
+//         author: "Shahjahan Tapan",
+//         marks: 30
+//     }
+// };//Count the number of properties.
+
+// let student = {
+//     name: 'Ariana Grande',
+//     age: 21,
+//     city: 'Gaibandha',
+//     isStudent: true
+// };
