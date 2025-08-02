c258e7ce15850f5ccf1a39166ba4b40287bffd25
procode99 <web.poran@gmail.com>
2025-08-03 00:32:07 +0600

    to  uppercase and toLowearCase

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-var hellojs = 'JavaScript';
-console.log(hellojs);
+// var hellojs = 'JavaScript';
+// //console.log(hellojs);
+// function myFunction (){
+//     document.getElementById("hello-java").innerHTML = "ClickHoGea me fast time";
+// }
+// const thana = new String('sakhipur');
+// // console.log(thana);
+// const number = [ 67, 55, 654, 78];
+// number[0] = 54;
+// console.log(number);
+// // console.log(number.length);
+// // console.log(number[3]);
+// let capital =' davkha ';
+// // console.log(capital);
+// // console.log(capital);
+// // capital[0]= Q ;
 
-function myFunction (){
-    document.getElementById("hello-java").innerHTML = "ClickHoGea me fast time";
+// // case pracktice 
+
+const college = 'Habib ullah college';
+const loocation = 'sakhipur shariatpur';
+
+// if (college.toLocaleLowerCase() === loocation.toLocaleLowerCase()){
+//     console.log('Ami bea korbona ');
+// }else{
+//     console.log('ami bea korbo');
+// }
+console.log(college.toUpperCase());
+
+
+const drink = ' water';
+const liquide = '  water ';
+if (drink.trim()===liquide.trim()){
+    console.log('pani pani hogea ')
+}
+else {
+    console.log('pani pani nahi melaga ')
 }
